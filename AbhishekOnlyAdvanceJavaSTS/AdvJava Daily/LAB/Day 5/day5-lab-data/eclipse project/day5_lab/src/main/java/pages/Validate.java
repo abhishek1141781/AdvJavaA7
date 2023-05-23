9be26e7fed5511842f06633144f05cfc7f0762cc
AbhishekOnLappy <abhishek1141781@gmail.com>
2023-05-23 23:14:03 +0530

    #5

---
+++ AbhishekOnlyAdvanceJavaSTS/AdvJava Daily/LAB/Day 5/day5-lab-data/eclipse project/day5_lab/src/main/java/pages/Validate.java
@@ -0,0 +1,12 @@
+package pages;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(value="/validate")
+public class Validate {
+//	day5_lab/validate?team_id=1&player_name=kohli&dob=2001-01-16&avg=88&wickets=15
+	
+	private static final long serialVersionUID = 1L;
+	private TeamDao
+
+}
